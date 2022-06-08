c4e3b7f9421ceb8c21a496f8a116ce250234c3ca
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-08 16:30:42 -0700

    write tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,25 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const RandomData = require('../lib/models/RandomData');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/random-data should return a list of random data', async () => {
+    const res = await request(app).get('/random-data');
+    const expected = await RandomData.getAll();
+    expect(res.body).toEqual(expected);
   });
+
+  it('/random-data/:id should return random item detail', async () => {
+    const res = await request(app).get('/random-data/1');
+    const expected = await RandomData.getById(1);
+    expect(res.body).toEqual(expected);
+  });
+
   afterAll(() => {
     pool.end();
   });
