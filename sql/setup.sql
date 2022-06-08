9ebe7eb82d70d34931a02027828bf84496e1116a
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-08 16:01:18 -0700

    some initial file setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists api_table;
+
+CREATE table api_table (
+  id INT GENERATED ALWAYS AS IDENTITY,
+);
+
+INSERT INTO api_table () VALUES 
