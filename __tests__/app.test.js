4cf4ff48463ee554ec73a1c66f735faa53b1074d
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-09 14:25:19 -0700

    remove error from test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const RandomData = require('../lib/models/RandomData');
 const { data } = require('../data/random-data');
 
 describe('backend-express-template routes', () => {
